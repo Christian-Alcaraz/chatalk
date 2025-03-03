04bb62c25b82f71240f25861bf6a684d8fbc67ca
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-03-03 22:57:22 +0800

    added button

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,58 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Color, ColorType } from '@core/constants';
+import { Size, SizeType } from '@core/constants/size.constant';
+
+type ButtonType = 'solid' | 'outline' | 'text';
+
+type FabButtonType = 'fab' | 'fab-outline' | 'fab-icon';
+
+interface BaseButtonProps {
+  label?: string;
+  color?: ColorType;
+  size?: SizeType;
+}
+
+interface NotFabButtonProps extends BaseButtonProps {
+  type?: ButtonType;
+  prefixIcon?: string; //font-awesome class
+  suffixIcon?: string; //font-awesome class
+}
+
+interface FabButtonProps extends BaseButtonProps {
+  type?: FabButtonType;
+  icon: string; //font-awesomeclass
+}
+
+export type ButtonProps = NotFabButtonProps | FabButtonProps;
+
+@Component({
+  selector: 'app-button',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent implements OnInit {
+  @Input() props?: ButtonProps;
+  @Input() disabled = false;
+
+  fabTypes = ['fab', 'fab-outline', 'fab-icon'];
+  buttonTypes = {
+    Solid: 'solid',
+    Outline: 'outline',
+    Text: 'text',
+    FabSolid: 'fab-solid',
+    FabOutline: 'fab-outline',
+    FabIcon: 'fab-icon',
+  };
+  color = Color;
+  size = Size;
+  type!: string;
+
+  @Output() onButtonClick = new EventEmitter<void>();
+
+  ngOnInit(): void {
+    this.type = this.props?.type ?? this.buttonTypes.Solid;
+  }
+}
