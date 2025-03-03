00b9b2fa0f4e7462167478ed6cef08dc2ffc3a06
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-03-03 17:08:24 +0800

    added basic feature components (auth, portal, admin portal)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadComponent: () =>
+      import('./features/auth/auth.component').then((c) => c.AuthComponent),
+  },
+  {
+    path: 'portal',
+    loadComponent: () =>
+      import('./features/portal/portal.component').then(
+        (c) => c.PortalComponent,
+      ),
+  },
+  {
+    path: 'admin-portal',
+    loadComponent: () =>
+      import('./features/admin-portal/admin-portal.component').then(
+        (c) => c.AdminPortalComponent,
+      ),
+  },
+];
