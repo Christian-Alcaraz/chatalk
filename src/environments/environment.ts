6ae2277f6f4652b63fc920f3d7ae11c694c7e755
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-07-10 23:14:50 +0800

    build: asdded ws url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@ const env = {
   ENVIRONMENT_NAME: 'development',
   PRODUCTION: false,
   API_URL: 'http://localhost:3000/api/v1',
+  WS_URL: 'ws://localhost:3000',
 };
 
 export const environment = {
